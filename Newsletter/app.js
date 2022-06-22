8eecffd66e6186a1daf31897f52c2f37c7d8fea4
Emanuele Zignale <zignale92@gmail.com>
2022-06-22 17:03:52 +0200

    versione base

---
+++ Newsletter/app.js
@@ -0,0 +1,72 @@
+
+//Need Key Api and pubblic id of audience of mailChimp:
+//https://mailchimp.com/it/
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require("request");
+const https = require('http');
+const { url } = require('inspector');
+const { post } = require('request');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/',function (req,res) {  
+     res.sendFile(__dirname+"/signup.html");
+});
+
+app.post('/',function(req,res){
+  
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name,
+          LNAME: lastname,
+          EMAIL: email
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  
+  const url = "https://usX.api.mailchimp.com/3.0/lists/{ Your PublicID }";
+
+  const options = {
+    method: "POST",
+    auth: "Your username:{your ApiKey }"
+  }
+
+  const request = https.request(url, options, function(response){
+    if(response.statusCode  === 200) 
+        res.sendFile(__dirname+"/success.html");
+      else
+        res.sendFile(__dirname+"/failure.html");
+    
+    response.on("data", function(data){
+      console.log(JSON.parse(data))
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post('/failure',function(req,res){
+  res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server Started: http://localhost:3000");
+})
+
